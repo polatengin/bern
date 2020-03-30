2f0adecd470cda08ff34a560c4025fed8c8ce430
Engin Polat <enpolat@microsoft.com>
2020-03-30 15:29:36 +0000

    landing page component imported

--- src/courier/src/app/app.module.ts
+++ src/courier/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 
+import { LandingPage } from './pages/landing/landing';
 @NgModule({
   declarations: [
   ],
