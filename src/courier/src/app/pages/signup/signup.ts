98b9fb61fed2a8ad9d48f36073d94082e79bb276
Engin Polat <enpolat@microsoft.com>
2020-03-31 09:14:39 +0000

    signup page component

---
+++ src/courier/src/app/pages/signup/signup.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ToastController } from '@ionic/angular';
+
+import { SignUpModel } from '../../models/signup.models';
+
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.html',
+  styleUrls: ['./signup.scss'],
+})
+export class SignUpPage implements OnInit {
+  constructor(private api: ApiService, private router: Router, private toastController: ToastController) { }
+
+  model: SignUpModel = new SignUpModel();
+
+  ngOnInit() {
+  }
+
+  createAccount() {
+    this.api.signup(this.model).subscribe(_ => {
+      this.router.navigate(['/main']);
+    }, async error => {
+      const toast = await this.toastController.create({
+        header: 'Oh No!',
+        message: "We couldn't create your account 😢",
+        position: 'top',
+        color: 'danger',
+        duration: 10 * 1000
+      });
+      toast.present();
+    });
+  }
+
+}
