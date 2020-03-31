dc032bdc33eb6ceee6b2e992231f1e88b95f7b9a
Engin Polat <enpolat@microsoft.com>
2020-03-31 09:06:22 +0000

    initial login page scripts

---
+++ src/courier/src/app/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ToastController } from '@ionic/angular';
+
+import { LoginModel } from '../../models/login.models';
+
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'],
+})
+export class LoginPage implements OnInit {
+  constructor(private api: ApiService, private router: Router, private toastController: ToastController) { }
+
+  model: LoginModel = new LoginModel();
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.api.login(this.model).subscribe(_ => {
+      this.router.navigate(['/main']);
+    }, async error => {
+      const toast = await this.toastController.create({
+        header: 'Oh No!',
+        message: "We couldn't logged you in 😢",
+        position: 'top',
+        color: 'danger',
+        duration: 10 * 1000
+      });
+      toast.present();
+    });
+  }
+}
