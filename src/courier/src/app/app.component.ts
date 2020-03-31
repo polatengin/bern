aeecd9416e2e07337a84103e04c8bf0e720f8079
Engin Polat <enpolat@microsoft.com>
2020-03-31 09:03:24 +0000

    unneccessary code removed

--- src/courier/src/app/app.component.ts
+++ src/courier/src/app/app.component.ts
@@ -51,10 +51,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
   }
 
   toggleDarkTheme(event) {
