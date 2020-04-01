bb3655eb239dfb22fe7eb767e4ca7170acd12557
Engin Polat <enpolat@microsoft.com>
2020-04-01 06:11:55 +0000

    delivery details page component

---
+++ src/courier/src/app/pages/delivery-details/delivery-details.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { Router } from '@angular/router';
+
+import { DeliveryListResponseModel } from '../../models/main.models';
+
+@Component({
+  selector: 'app-delivery-details',
+  templateUrl: './delivery-details.html',
+  styleUrls: ['./delivery-details.scss'],
+})
+export class DeliveryDetailsPage implements OnInit {
+  constructor(private location: Location, private router: Router) { }
+
+  public model: DeliveryListResponseModel = new DeliveryListResponseModel();
+
+  ngOnInit() {
+    let state = this.router.getCurrentNavigation().extras.state;
+    if (state) {
+      this.model = state.item;
+    }
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}
