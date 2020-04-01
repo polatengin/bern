04858b70b43bfc008ec1d18b501233c3e19ffe07
Engin Polat <enpolat@microsoft.com>
2020-04-01 05:58:04 +0000

    delivery response model

--- src/courier/src/app/models/main.models.ts
+++ src/courier/src/app/models/main.models.ts
@@ -3,3 +3,14 @@ export class DeliveryListRequestModel {
   longitude: number;
   range: number;
 }
+
+export class DeliveryListResponseModel {
+  latitude: number;
+  longitude: number;
+  range: number;
+  address: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  dueDate: Date
+}
