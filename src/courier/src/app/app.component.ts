22ad0e29ee97f5de69969eaa15b7133afb9892ef
polatengin <enpolat@microsoft.com>
2020-04-05 10:03:54 +0300

    default selected menu index set

--- src/courier/src/app/app.component.ts
+++ src/courier/src/app/app.component.ts
@@ -10,22 +10,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public selectedIndex: number = 0;
+
   public menuItems = [{
-      title: 'Search for Deliveries',
-      url: '/main',
-      icon: 'search-outline'
+    title: 'Search for Deliveries',
+    url: '/main',
+    icon: 'search-outline'
   }, {
-      title: 'Profile',
-      url: '/profile',
-      icon: 'person-outline'
+    title: 'Profile',
+    url: '/profile',
+    icon: 'person-outline'
   }, {
-      title: 'Delivery history',
-      url: '/delivery-history',
-      icon: 'cube-outline'
+    title: 'Delivery history',
+    url: '/delivery-history',
+    icon: 'cube-outline'
   }, {
-      title: 'Reports',
-      url: '/reports',
-      icon: 'document-text-outline'
+    title: 'Reports',
+    url: '/reports',
+    icon: 'document-text-outline'
   }, {
     title: 'Support',
     url: '/support',
