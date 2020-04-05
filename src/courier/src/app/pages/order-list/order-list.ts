81fb973043cc59418584b61d5efc68df51a3676e
polatengin <enpolat@microsoft.com>
2020-04-05 10:34:51 +0300

    re-format document

--- src/courier/src/app/pages/order-list/order-list.ts
+++ src/courier/src/app/pages/order-list/order-list.ts
@@ -17,9 +17,11 @@ import { OrderResponseModel } from '../../models/order-list.models';
 })
 export class OrderListPage implements OnInit {
   constructor(private api: ApiService, private barcodeScanner: BarcodeScanner, private location: Location, private router: Router) { }
+
   public model: DeliveryListResponseModel = new DeliveryListResponseModel();
 
   public response: OrderResponseModel[] = [];
+
   ngOnInit() {
     let state = this.router.getCurrentNavigation().extras.state || {};
 
@@ -29,12 +31,15 @@ export class OrderListPage implements OnInit {
       this.response = _;
     });
   }
+
   goBack() {
     this.location.back();
   }
+
   done() {
     this.location.back();
   }
+
   scanBarcode() {
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
@@ -42,4 +47,5 @@ export class OrderListPage implements OnInit {
       console.log('Error', err);
     });
   }
+
 }
