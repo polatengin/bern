be7b2993c70d553c8473d37f6a5d42f02d8df3ce
polatengin <enpolat@microsoft.com>
2020-04-05 10:03:29 +0300

    menu items cleaned up

--- src/courier/src/app/app.component.ts
+++ src/courier/src/app/app.component.ts
@@ -10,43 +10,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public menuItems = [
-    {
+  public menuItems = [{
       title: 'Search for Deliveries',
       url: '/main',
       icon: 'search-outline'
-    },
-    {
+  }, {
       title: 'Profile',
       url: '/profile',
       icon: 'person-outline'
-    },
-    {
+  }, {
       title: 'Delivery history',
       url: '/delivery-history',
       icon: 'cube-outline'
-    },
-    {
+  }, {
       title: 'Reports',
       url: '/reports',
       icon: 'document-text-outline'
-    },
-    {
-      title: 'Shopping List',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Summary',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Order Details',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
-  ];
+  }, {
+    title: 'Support',
+    url: '/support',
+    icon: 'help-circle-outline'
+  }];
 
   constructor(platform: Platform, splashScreen: SplashScreen, statusBar: StatusBar) {
     platform.ready().then(() => {
